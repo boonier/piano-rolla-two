88ade2afb11380fa02d167b58ea537f074c517f1
Si Mills <simills@Sis-MacBook-Pro.local>
2016-10-31 23:06:22 +0000

    Add svg stub

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
+	
+	static defaultProps = {
+		x: 50,
+		y: 50,
+		r: 20,
+		fill: 'red'
+	}
+	
+	render() {
+		return (
+			<svg xmlns="http://www.w3.org/2000/svg">
+				<circle cx={this.props.x} cy={this.props.y} r={this.props.r} fill={this.props.fill} />
+			</svg>
+		);
+	}
 }
